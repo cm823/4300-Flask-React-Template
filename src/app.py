3fbfb4cc108dfde43af0c947c053dc69c8b344ee
Kevin Peng <kcp52@cornell.edu>
2026-03-17 15:50:36 -0400

    test push

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 from models import db, Episode, Review
 from routes import register_routes
 
+# test comment
 # src/ directory and project root (one level up)
 current_directory = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_directory)
