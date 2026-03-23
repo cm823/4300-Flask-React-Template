0937ea1d3c2c33f09c2815c5e8abc713106bd2e4
Kevin Peng <kcp52@cornell.edu>
2026-03-23 18:18:26 -0400

    db now connects from var/www

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ CORS(app)
 db_path = os.path.join(project_root, 'var/www/inverted_index.db')
 print("DB PATH:", db_path)
 print("Exists?", os.path.exists(db_path))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///var/www/inverted_index.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite://var/www/inverted_index.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
