46d16be96621120459a2b448dacd3e58eaefa7ba
Kevin Peng <kcp52@cornell.edu>
2026-04-12 22:05:23 -0400

    svd bugfixes and backend for singular value explainability

--- src/utils.py
+++ src/utils.py
@@ -12,11 +12,115 @@ stemmer = PorterStemmer()
 stemmed_stopwords = list({stemmer.stem(w) for w in ENGLISH_STOP_WORDS}) \
                      + ['anywh', 'becau', 'el', 'elsewh', 'everywh', 'ind', 'otherwi', 'plea', 'somewh']
 
+dimension_themes = {
+    0:  "Film & Television",
+    1:  "Football & Team Sports",
+    2:  "Music",
+    3:  "Music",
+    4:  "Film & Television",
+    5:  "Football & Team Sports",
+    6:  "Football & Team Sports",
+    7:  "Combat Sports & Racing",
+    8:  "Baseball & Ice Hockey",
+    9:  "Football & Team Sports",
+    10: "Music",
+    11: "Film & Television",
+    12: "Football & Team Sports",
+    13: "Baseball & Ice Hockey",
+    14: "Baseball & Ice Hockey",
+    15: "Indian Cinema & South Asian",
+    16: "Film & Television",
+    17: "Football & Team Sports",
+    18: "Animation, Gaming & Comics",
+    19: "Combat Sports & Racing",
+    20: "Combat Sports & Racing",
+    21: "Literature & Theatre",
+    22: "Literature & Theatre",
+    23: "Olympics & Athletics",
+    24: "Music",
+    25: "Combat Sports & Racing",
+    26: "Animation, Gaming & Comics",
+    27: "Combat Sports & Racing",
+    28: "Politics & Government",
+    29: "Nationality",
+    30: "Combat Sports & Racing",
+    31: "Film & Television",
+    32: "Broadcasting & Journalism",
+    33: "Combat Sports & Racing",
+    34: "Combat Sports & Racing",
+    35: "Broadcasting & Journalism",
+    36: "Animation, Gaming & Comics",
+    37: "Nationality",
+    38: "Literature & Theatre",
+    39: "Film & Television",
+    40: "Nationality",
+    41: "Nationality",
+    42: "Animation, Gaming & Comics",
+    43: "Nationality",
+    44: "Film & Television",
+    45: "Indian Cinema & South Asian",
+    46: "Nationality",
+    47: "Film & Television",
+    48: "Literature & Theatre",
+    49: "Nationality",
+    50: "Nationality",
+    51: "Music",
+    52: "Nationality",
+    53: "Nationality",
+    54: "Nationality",
+    55: "Broadcasting & Journalism",
+    56: "Indian Cinema & South Asian",
+    57: "Nationality",
+    58: "Film & Television",
+    59: "Literature & Theatre",
+    60: "Broadcasting & Journalism",
+    61: "Literature & Theatre",
+    62: "Nationality",
+    63: "Broadcasting & Journalism",
+    64: "Politics & Government",
+    65: "Nationality",
+    66: "Broadcasting & Journalism",
+    67: "Animation, Gaming & Comics",
+    68: "Broadcasting & Journalism",
+    69: "Broadcasting & Journalism",
+    70: "Indian Cinema & South Asian",
+    71: "Politics & Government",
+    72: "Literature & Theatre",
+    73: "Politics & Government",
+    74: "Nationality",
+    75: "Nationality",
+    76: "Literature & Theatre",
+    77: "Olympics & Athletics",
+    78: "Literature & Theatre",
+    79: "Olympics & Athletics",
+    80: "Animation, Gaming & Comics",
+    81: "Olympics & Athletics",
+    82: "Animation, Gaming & Comics",
+    83: "Olympics & Athletics",
+    84: "Literature & Theatre",
+    85: "Nationality",
+    86: "Olympics & Athletics",
+    87: "Olympics & Athletics",
+    88: "Nationality",
+    89: "Nationality",
+    90: "Nationality",
+    91: "Nationality",
+    92: "Nationality",
+    93: "Olympics & Athletics",
+    94: "Nationality",
+    95: "Nationality",
+    96: "Olympics & Athletics",
+    97: "Nationality",
+    98: "Politics & Government",
+    99: "Nationality",
+}
+
 DOC_MAP = {}
 REVERSE_DOC_MAP = {}
 WORD_MAP = {}
 DOC_EMBEDDINGS = np.zeros(1)
 TERM_EMBEDDINGS = np.zeros(1)
+SINGULAR_VALUES = np.zeros(1)
 DOC_IDS_SVD = {}
 
 logger = logging.getLogger(__name__)
@@ -50,13 +154,14 @@ project_root = os.path.dirname(current_directory)
 DATA_DIR = os.path.join(project_root, 'data')
 
 def load_data():
-    global DOC_MAP, REVERSE_DOC_MAP, WORD_MAP, DOC_EMBEDDINGS, TERM_EMBEDDINGS, DOC_IDS_SVD
+    global DOC_MAP, REVERSE_DOC_MAP, WORD_MAP, DOC_EMBEDDINGS, TERM_EMBEDDINGS, DOC_IDS_SVD, SINGULAR_VALUES
     doc_path = os.path.join(DATA_DIR, "doc_map2.json")
     word_path = os.path.join(DATA_DIR, "word_map2.json")
     doc_embeddings_path_1 = os.path.join(DATA_DIR, "svd_scipy", "doc_embeddings1.npy")
     doc_embeddings_path_2 = os.path.join(DATA_DIR, "svd_scipy", "doc_embeddings2.npy")
     term_embeddings_path = os.path.join(DATA_DIR, "svd_scipy", "term_embeddings.npy")
     doc_ids_path = os.path.join(DATA_DIR, "svd_scipy", "doc_ids.txt")
+    singular_values_path = os.path.join(DATA_DIR, "svd_scipy", "singular_values.npy")
 
     try:
         with open(doc_path, "r") as f:
@@ -80,6 +185,8 @@ def load_data():
     arr2 = np.load(doc_embeddings_path_2, allow_pickle=True)
     DOC_EMBEDDINGS = np.concatenate([arr1, arr2], axis=0)
     TERM_EMBEDDINGS = np.load(term_embeddings_path)
+    SINGULAR_VALUES = np.load(singular_values_path)
+
 
     with open(doc_ids_path) as f:
         for line in f:
@@ -162,11 +269,13 @@ def generate_rabbit_hole(start_article, additional_keywords, postings_model, pat
             candidates.remove(best_doc)
     
     # 3. Format output 
+    # Changed to branch nodes as frontend expects many branch nodes for each rabbit hole. 
     branch_nodes = []
+
     description = "A unique thematic cluster."
-    
+
     np.random.shuffle(pathway)
-    
+
     for i in range(0, path_length*num_branches, path_length):
         nodes = pathway[i:i+path_length]
         temp = []
@@ -185,22 +294,51 @@ def generate_rabbit_hole(start_article, additional_keywords, postings_model, pat
             })
         if temp:
             branch_nodes.append(temp)
-    
+
     return branch_nodes
 
 
+def generate_branch(doc_id, doc_vec, doc_embed, term_embed, path_length=5):
+    path = [doc_id]
+    prev_doc = doc_vec
+    j = 0
+    while len(path) < path_length and j < 20:
+        q_emb = prev_doc @ term_embed
+        q_emb /= np.linalg.norm(q_emb) + 1e-8
+        scores = doc_embed @ q_emb
+        top_idx = np.argsort(scores)[-5:][::-1]
+        np.random.shuffle(top_idx)
+
+        i = 0
+        if top_idx[0] in path:
+            i += 1
+
+        path.append(top_idx[i])
+        prev_doc = TERM_EMBEDDINGS @ (SINGULAR_VALUES * DOC_EMBEDDINGS[top_idx[i], :])
+        j += 1
+
+    return path
+
+def top_query_dimensions(q_emb, top_k=5):
+    # sort by magnitude (important: absolute value)
+    idx = np.argsort(np.abs(q_emb))[::-1][:top_k]
+
+    return [(i, q_emb[i]) for i in idx]
+
 def generate_rabbit_hole_svd(start_article, path_length=5, num_branches=3):
-    global TERM_EMBEDDINGS, DOC_EMBEDDINGS, DOC_IDS_SVD
+    global TERM_EMBEDDINGS, DOC_EMBEDDINGS, DOC_IDS_SVD, SINGULAR_VALUES
 
     tokens = stem_tokenizer(start_article)
     unique_tokens = list(set(tokens))
 
     num_terms = TERM_EMBEDDINGS.shape[0]
     vec = np.zeros(num_terms, dtype=np.float32)
+    print(vec.shape)
 
     for w in unique_tokens:
         if w in WORD_MAP:
             vec[WORD_MAP[w]] += 1.0
+
 
     q_emb = vec @ TERM_EMBEDDINGS
 
@@ -216,17 +354,27 @@ def generate_rabbit_hole_svd(start_article, path_length=5, num_branches=3):
 
     description = "A unique thematic cluster."
 
-    for i in range(0, path_length*(num_branches), path_length):
+    for i in range(0, path_length * num_branches, path_length):
         nodes = top_idx[i:i+path_length]
         temp = []
         for node in nodes:
+            doc_vec = TERM_EMBEDDINGS @ (SINGULAR_VALUES * DOC_EMBEDDINGS[top_idx[i], :])
+            dims = top_query_dimensions(doc_vec @ TERM_EMBEDDINGS)
+            dim_names = []
+            dim_scores = []
+            for dim, score in dims:
+                if dimension_themes[dim] not in dim_names:
+                    dim_names.append(dimension_themes[dim])
+                    dim_scores.append(float(score))
             temp.append(
                 {
                     "id": int(node),
                     "title": DOC_IDS_SVD[node],
                     "score": round(float(scores[node]), 4),
                     "branch": int(i/path_length)+1,
-                    "description": description
+                    "description": description,
+                    "dimensions": dim_names,
+                    "dimensionScores": dim_scores
                 }
             )
         branch_nodes.append(temp)
