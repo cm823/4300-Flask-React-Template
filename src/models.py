b372f3626badc508fa64b290557d56fb95628401
Kevin Peng <kcp52@cornell.edu>
2026-04-22 20:55:07 -0400

    added articles table model

--- src/models.py
+++ src/models.py
@@ -6,3 +6,10 @@ class Postings(db.Model):
     __tablename__ = 'postings' 
     term_id = db.Column(db.Integer, primary_key=True) 
     postings = db.Column(db.LargeBinary, nullable=False)
+
+class Articles(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer)
+    article_name = db.Column(db.String)
+    article_text = db.Column(db.Text)
